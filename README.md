5f277ad5f80f42499f37a0597994c9148dd64707
Max <zhh064@ucsd.edu>
2025-05-27 13:07:08 -0700

    submit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab8-Starter
+- Name: Max Huang
+- [Link](https://max-dwd.github.io/Lab8_Starter/)
+- Graceful degradation is all about ensuring the site still works—even if some advanced features aren’t available—by falling back to simpler behavior. Service workers embody this principle by intercepting network requests: if the user is offline or a resource fails to load, the worker can return a cached copy instead. In this way, service workers provide a built-in fallback layer that keeps the application functional and responsive when the network or certain APIs aren’t available, exemplifying graceful degradation in web apps.
+![pwa](/pwa.png)
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,28 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if (!('serviceWorker' in navigator)) return;
   // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
+  window.addEventListener('load', ()=> {
+    navigator.serviceWorker.register('./sw.js')
+    .then(registration => {
+      console.log('Service Worker registered!', registration.scope);
+    })
+    .catch(err => {
+      console.error('Service Worker registration failed!', err);
+    })
+  })
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
+  
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  
+  
 }
 
 /**
@@ -68,21 +81,37 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const cached = localStorage.getItem('recipes');
+  if (cached) return JSON.parse(cached);
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
-  /**************************/
+  return new Promise(async (resolve, reject) => {
+    try {
+      for (const url of RECIPE_URLS) {
+        const response = await fetch(url);
+        const recipe = await response.json();
+        recipes.push(recipe);
+      }
+      saveRecipesToStorage(recipes);
+      resolve(recipes);
+    } catch (err) {
+      console.log('Error fetching the recipes:', err);
+    }
+    /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
+  
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
   //            a try / catch block. A6-A9 will be in the try portion, A10-A11
   //            will be in the catch portion.
@@ -100,6 +129,8 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+  })
+  
 }
 
 /**
